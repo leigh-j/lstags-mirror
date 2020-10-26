200c3b97291d30380e046389721451ec5f91c7cf
EC2 Default User <ec2-user@ip-172-31-30-153.ap-southeast-2.compute.internal>
2020-10-26 05:09:45 +0000

    initial commit

--- mirror.sh
+++ mirror.sh
@@ -4,4 +4,5 @@ export LSTAGS_ARGS="-j /var/lib/import/plays/auth.json -P --push-registry=regist
 lstags -f k8s.gcr.io.yml --push-prefix=gcr.io/k8s-artifacts-prod $LSTAGS_ARGS
 lstags -f gcr.io.yml --push-prefix=gcr.io $LSTAGS_ARGS
 lstags -f redhat.io.yml --push-prefix=registry.redhat.io $LSTAGS_ARGS
+# some of these images have hundreds of tags, some more considered selection is required
 #lstags -f docker.io.yml --push-prefix=docker.io $LSTAGS_ARGS
