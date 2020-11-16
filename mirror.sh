247b1eba45d67548849fcc63ff233b28ac64b8db
fedora Cloud User <fedora@ip-10-16-77-210.ap-southeast-2.compute.internal>
2020-11-16 22:58:54 +0000

    added all new images

--- mirror.sh
+++ mirror.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-export LSTAGS_ARGS="-j /var/lib/import/plays/auth.json -P --push-registry=registry.therealworld:7000 -c6"
+export LSTAGS_ARGS="-j /var/lib/import/plays/auth.json -P --push-registry=registry.therealworld:7000 -c1 -k"
 lstags -f k8s.gcr.io.yml --push-prefix=gcr.io/k8s-artifacts-prod $LSTAGS_ARGS
 lstags -f gcr.io.yml --push-prefix=gcr.io $LSTAGS_ARGS
 lstags -f redhat.io.yml --push-prefix=registry.redhat.io $LSTAGS_ARGS
-# some of these images have hundreds of tags, some more considered selection is required
-#lstags -f docker.io.yml --push-prefix=docker.io $LSTAGS_ARGS
+lstags -f docker.io.yml --push-prefix=docker.io $LSTAGS_ARGS
